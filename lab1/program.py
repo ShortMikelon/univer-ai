2e19ea9d91df0d5e169f9c0c14cdbc223b956567
ShortMikelon <kenesaset485@gmail.com>
2024-09-11 22:14:26 +0500

    Bugs fix

--- lab1/program.py
+++ lab1/program.py
@@ -1,4 +1,19 @@
 import pandas as pd
+import re
+
+def extract_first_name(full_name):
+    match = re.search(r'\((.*?)\)', full_name)
+
+    if match:
+        return match.group(1).split()[0]
+    
+    parts = full_name.split(',')
+    name_part = parts[1].strip().split('. ')[1]
+
+    if ' ' in name_part:
+        return name_part.split()[0]
+    else:
+        return name_part
 
 data = pd.read_csv("Титаник.csv")
 
@@ -11,9 +26,15 @@ print(f'Доля выживших пассажиров: {round(survived_percent[
 class_passenger_percent = data["Pclass"].value_counts(normalize=True)
 print(f'Доля пассажиров путешествующих в 2 классе: {round(class_passenger_percent[2] * 100, 2)}')
 
-print(f'Среднее значение возраста пассажирож: {data["Age"].mean()}')
+print(f'Среднее значение возраста пассажирож: {round(data["Age"].mean(), 2)}')
 
 print(f'Медианная значение возраста пассажирож: {data["Age"].median()}')
 
 correlotion = data["SibSp"].corr(data['Parch'])
 print(f'Коррелицая число братьев/сестер с числом родителей/детей: {round(correlotion, 2)}')
+
+female_names = data[data["Sex"] == 'female']['Name']
+female_names = female_names.apply(extract_first_name)
+
+popular_female_name, popular_female_name_count = next(female_names.value_counts(sort=True).items())
+print(f'Самый популярное женская имя: {popular_female_name}, количество: {popular_female_name_count}')
